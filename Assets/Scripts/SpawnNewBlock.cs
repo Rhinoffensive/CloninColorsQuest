51c39a638a0182809ec89f7fe5fbec3bc2e1591f
Rπ <renantaylan@gmail.com>
2021-11-21 17:15:28 +0300

    Modify our SpawnNewBlock.cs script to instantiate a block prefab to one of our 3 random colors to a position of your choice. The SpawnBlock() method is already rigged up to our UI button.

--- Assets/Scripts/SpawnNewBlock.cs
+++ Assets/Scripts/SpawnNewBlock.cs
@@ -1,6 +1,7 @@
 ﻿// GameDev.tv ChallengeClub.Got questionsor wantto shareyour niftysolution?
 // Head over to - http://community.gamedev.tv
 
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,6 +13,16 @@ public class SpawnNewBlock : MonoBehaviour
 
     public void SpawnBlock()
     {
+        System.Array values = System.Enum.GetValues(typeof(PresetColor));
+        int selection = Random.Range(0, values.Length);
+        PresetColor color = (PresetColor)values.GetValue(selection);
+
+        Color theColor;
+        if (ColorUtility.TryParseHtmlString(color.ToString(), out theColor))
+        {
+
+            blockPrefab.GetComponent<SpriteRenderer>().color = theColor;
+        }
         Instantiate(blockPrefab, spawnPosition.position, Quaternion.identity);
     }
 }
