88b462c0d2cea691548e31b5c79801cc58b16280
Rπ <renantaylan@gmail.com>
2021-11-23 22:28:07 +0300

    Gem color copy prop from parrent may not a great idea.

--- Assets/Scripts/PaintToucher.cs
+++ Assets/Scripts/PaintToucher.cs
@@ -4,7 +4,34 @@ using UnityEngine;
 
 public class PaintToucher : MonoBehaviour
 {
-  
+    private GameObject parrent;
+    private PresetColor gem_color;
+    private SpriteRenderer getSpriteRenderer;
+
+    private void Awake()
+    {
+
+
+        {
+            var sr_p = parrent.GetComponentInParent<SpriteRenderer>();
+            var parrent_color = sr_p.color;
+            GetComponent<SpriteRenderer>().color = parrent_color;
+            var block_color = GetComponent<ColorChanger>().color;
+            var colEnum = GetComponent<SpriteRenderer>();
+
+            Color theColor;
+            if (ColorUtility.TryParseHtmlString(block_color.ToString(), out theColor))
+            {
+                sr_p.color = theColor;
+                // = System.Enum.GetName(typeof(PresetColor), block_color);
+            }
+
+
+
+
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Block")
